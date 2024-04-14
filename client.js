265973cfe4b545bcf59c4b5bba8ebd2650f8fc66
Forgottenit <109222348+Forgottenit@users.noreply.github.com>
2024-04-14 19:45:10 +0100

    Added Temperature Server side stream rpc

--- client.js
+++ client.js
@@ -12,14 +12,26 @@ const client = new farmProto.FarmManagement(
   "localhost:50051",
   grpc.credentials.createInsecure()
 );
-
+// GetWaterLevel function - retrieves the water level for sensor ID
 function getWaterLevel(sensorId, callback) {
   client.GetWaterLevel({ sensorId: sensorId }, (error, response) => {
     if (error) {
       console.error("Error fetching water level:", error);
     } else {
-      console.log(`Water level: ${response.level}`);
+      console.log(`Water level: ${response.level} inches`);
     }
+    callback(); // Call callback after operation is complete
+  });
+}
+
+// MonitorTemperature function - streams temperature data
+function monitorTemperature(callback) {
+  const call = client.MonitorTemperature({});
+  call.on("data", (response) => {
+    console.log(`Temperature: ${response.temperature}`);
+  });
+  call.on("end", () => {
+    console.log("Stream ended.");
     callback(); // Call callback after operation is complete
   });
 }
@@ -32,10 +44,11 @@ const rl = readline.createInterface({
 function main() {
   console.log("Select an Operation:");
   console.log("1. Get Water Level");
-  console.log("2. Exit");
+  console.log("2. Monitor Temperature");
+  console.log("6. Exit");
 
   rl.question("Enter your choice: ", function (choice) {
-    if (choice === "2") {
+    if (choice === "6") {
       console.log("Exiting...");
       rl.close();
     } else {
@@ -50,6 +63,9 @@ function processChoice(choice, callback) {
   switch (choice) {
     case "1":
       getWaterLevel("sensor123", callback);
+      break;
+    case "2":
+      monitorTemperature(callback);
       break;
 
     default:
--- server.js
+++ server.js
@@ -13,7 +13,16 @@ server.addService(farmProto.FarmManagement.service, {
   // Implement Unary RPC
   GetWaterLevel: (call, callback) => {
     console.log("Fetching water level for sensor:", call.request.sensorId);
-    callback(null, { level: 42 });
+    callback(null, { level: 2 });
+  },
+
+  // Implement Server-side Streaming RPC
+  MonitorTemperature: (call) => {
+    const temperatures = [20, 21, 22, 23, 24, 25];
+    temperatures.forEach((temp) => {
+      call.write({ temperature: temp });
+    });
+    call.end();
   },
 });
 
